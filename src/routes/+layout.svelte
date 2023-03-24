638cb29600b1eeba020699e15e4510d890f5971c
Chris <chris9soul@gmail.com>
2023-03-24 06:43:28 +0200

    added head title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import '../app.css'
 </script>
 
+<svelte:head>
+	<title>BoredGPT | by Chris</title>
+</svelte:head>
+
 <div class="flex flex-col items-center max-w-2xl mx-auto">
 	<slot />
 </div>
