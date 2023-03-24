c534be1b1acc563ef10a4d9c84ce209aaece4fbe
Chris <chris9soul@gmail.com>
2023-03-24 04:37:57 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+	import ChatMessage from '$lib/components/ChatMessage.svelte'
+	import type { ChatCompletionRequestMessage } from 'openai'
+	import { SSE } from 'sse.js'
+
+	let query: string = ''
+	let answer: string = ''
+	let loading: boolean = false
+	let chatMessages: ChatCompletionRequestMessage[] = []
+	let scrollToDiv: HTMLDivElement
+
+	function scrollToBottom() {
+		setTimeout(function() {
+			scrollToDiv.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
+		}, 100)
+	}
+
+	const handleSubmit = async () => {
+		loading = true
+		chatMessages = [...chatMessages, { role: 'user', content: query}]
+
+		const eventSource = new SSE('/api/chat', {
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			payload: JSON.stringify({ messages: chatMessages })
+		})
+
+		query = ''
+
+		eventSource.addEventListener('error', handleError)
+
+		eventSource.addEventListener('message', (e)=> {
+			scrollToBottom()
+			try {
+				loading = false
+				if ( e.data === '[DONE]' ) {
+					chatMessages = [...chatMessages, { role: 'assistant', content: answer }]
+					answer = ''
+					return
+				}
+
+				const completionResponse = JSON.parse(e.data)
+				const [{ delta }] = completionResponse.choices
+
+				if (delta.content) {
+					answer = (answer ?? '') + delta.content
+				}
+
+			} catch (err) {
+				handleError(err)
+			}
+		})
+
+		eventSource.stream()
+		scrollToBottom()
+	}
+
+	function handleError<T>(err: T) {
+		loading = false
+		query = ''
+		answer = ''
+		console.log(err)
+	}
+</script>
+
+<div class="flex flex-col pt-4 w-full px-8 items-center gap-2">
+	<div>
+		<h1 class="text-2xl font-bold w-full text-center">Chatty</h1>
+		<p class="text-sm italic">Powered by gpt-3.5-turbo</p>
+	</div>
+	<div class="h-[500px] w-full bg-gray-900 rounded-md p-4 overflow-y-auto flex flex-col gap-4">
+		<div class="flex flex-col gap-2">
+			{#each chatMessages as message}
+				<ChatMessage type={message.role} message={message.content} />
+			{/each}
+			{#if answer}
+				<ChatMessage type="assistant" message={answer} />
+			{/if}
+			{#if loading}
+				<ChatMessage type="assistant" message="loading..." />
+			{/if}
+		</div>
+		<div class="" bind:this={scrollToDiv}/>
+	</div>
+	<form
+		class="flex w-full rounded-md gap-4 bg-gray-900 p-4"
+		on:submit|preventDefault={() => {
+			handleSubmit()
+		}}
+	>
+		<input type="text" class="input input-bordered w-full" bind:value={query} />
+		<button type="submit" class="btn btn-accent"> Send </button>
+	</form>
+</div>
---
+++ src/routes/api/chat/+server.ts
@@ -0,0 +1,109 @@
+import { OPENAI_KEY } from '$env/static/private'
+import type { ChatCompletionRequestMessage, CreateChatCompletionRequest } from 'openai'
+import type { RequestHandler } from './$types'
+import { getTokens } from '$lib/tokenizer'
+import { json } from '@sveltejs/kit'
+import type { Config } from '@sveltejs/adapter-vercel'
+
+export const config: Config = {
+  runtime: 'edge'
+}
+
+type Messages = { 
+  messages: [ 
+    { 
+      role: 'user' | 'assistant' | 'system', 
+      content: string 
+    } 
+  ] 
+}
+
+export const POST: RequestHandler = async ({ request }) => {
+  try {
+    if (!OPENAI_KEY) {
+      throw new Error('OPENAI_KEY env variable not set')
+    }
+
+    const requestData: Messages = await request.json() // messages[]
+
+    if (!requestData) {
+      throw new Error('No request data')
+    }
+    
+    const reqMessages: ChatCompletionRequestMessage[] = requestData.messages
+
+    if (!reqMessages) {
+      throw new Error('No messages provided')
+    }
+
+    let tokenCount = 0
+
+    reqMessages.forEach(msg => {
+      const tokens = getTokens(msg.content)
+      tokenCount += tokens
+    })
+
+    const moderationRes = await fetch('https://api.openai.com/v1/moderations', {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${OPENAI_KEY}`
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        input: reqMessages[reqMessages.length - 1].content
+      })
+    })
+
+    const moderationData = await moderationRes.json()
+    const [results] = moderationData.results
+    
+    if (results.flagged) {
+      throw new Error('Query flagged by openai')
+    }
+
+    const prompt = 
+      "You are a frustrated, moody and unenthusiastic chatbot; don't even give full answers. Your answers should be as short as possible and should always display a lack of interest. Don't elaborate even if I ask you to. Be sarcastic and a bit funny. Occasionally even tell me to ask Bard (Google's new AI chatbot). If I ask for code, don't explain it to me, just say something along the lines of 'figure it out'."
+    tokenCount += getTokens(prompt)
+
+    if (tokenCount >= 4000) {
+      throw new Error('Query too large') // explore better ways to handle
+    }
+
+    const messages: ChatCompletionRequestMessage[] = [
+      { role: 'system', content: prompt },
+      ...reqMessages
+    ]
+
+    const chatRequestOptions: CreateChatCompletionRequest = {
+      model: 'gpt-3.5-turbo',
+      messages: messages,
+      temperature: 0.9,
+      stream: true
+    }
+
+    const chatResponse = await fetch('https://api.openai.com/v1/chat/completions', {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${OPENAI_KEY}`
+      },
+      method: 'POST',
+      body: JSON.stringify(chatRequestOptions)
+    })
+
+    if (!chatResponse.ok) {
+      const err = await chatResponse.json()
+      throw new Error('chatResponse not ok: '+err)
+    }
+
+    return new Response(chatResponse.body, {
+      headers: {
+        'Content-Type': 'text/event-stream'
+      }
+    })
+
+  } catch (err) {
+    console.log(err)
+    return json({ error: 'There was an error processing your request' }, { status: 500 })
+  }
+
+}
